73b4bfc32a3caeeab891c68353e4ee240a3146b2
Mellfine07 <126507109+Mellfine07@users.noreply.github.com>
2024-05-07 01:38:01 +0000

    06/05/24

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ function atualizarContador() {
   const hoje = new Date();
   const intervaloTempo = dataFinal - hoje;
 
-  if(intervaloTempo <= 0) {
+  if (intervaloTempo <= 0) {
     contador.textContent = "Promoção Encerrada!";
   } else {
     const dias = Math.floor(intervaloTempo / (1000 * 60 * 60 * 24))
 
     const horas = Math.floor((intervaloTempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
-   const minutos = Math.floor((intervaloTempo % (1000 * 60 * 60) / (1000 * 60)));
+    const minutos = Math.floor((intervaloTempo % (1000 * 60 * 60) / (1000 * 60)));
 
     const segundos = Math.floor((intervaloTempo % (1000 * 60)) / 1000);
 
     contador.textContent = dias + "d " + horas + "h " + minutos + "min " + segundos + "s";
-    
+
   }
 }
 
